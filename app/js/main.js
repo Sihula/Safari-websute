452372483fda4d005ecd39562698fb3b357a0441
Sihula <serhiisihula@gmail.com>
2021-11-29 16:30:18 +0100

    html ready

---
+++ app/js/main.js
@@ -0,0 +1,17 @@
+$(function () {
+
+  $('.slidergalary__inner').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    arrows: true,
+    // autoplay: true
+  });
+
+
+
+
+
+
+
+});
