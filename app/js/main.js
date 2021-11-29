d9242ebd06da394352c63e2934a39d7829f3d9b1
Sihula <serhiisihula@gmail.com>
2021-11-29 23:58:10 +0100

    end adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -5,10 +5,30 @@ $(function () {
     slidesToShow: 3,
     slidesToScroll: 1,
     arrows: true,
-    // autoplay: true
+    autoplay: true,
+    responsive: [{
+        breakpoint: 801,
+        settings: {
+          slidesToShow: 2,
+          arrows: false
+        }
+      },
+      {
+        breakpoint: 501,
+        settings: {
+          slidesToShow: 1,
+          arrows: false
+        }
+      },
+
+    ]
+
   });
 
+  $('.burger__menu').on('click', function () {
+    $('.header__menu').toggleClass('active');
 
+  });
 
 
 
